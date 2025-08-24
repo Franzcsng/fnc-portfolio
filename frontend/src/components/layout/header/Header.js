f13702dee85513929c0c7a065098a306518db3e4
Francis Causing <franzcsng23@gmail.com>
2025-08-25 00:05:58 +0800

    Fixed header content positioning and footer container responsiveness

--- frontend/src/components/layout/header/Header.js
+++ frontend/src/components/layout/header/Header.js
@@ -9,22 +9,25 @@ const Header = ({toggleMode, mode}) => {
 
 
     return (
-        <div className='header'>
-            <div className='logo'></div>    
-            <nav>
-                <ul>
-                    <li><NavLink to='/'>home</NavLink></li>
-                    <li><NavLink to='/about'>about</NavLink></li>
-                    <li><NavLink to='/projects'>projects</NavLink></li>
-                    <li><NavLink to='/resume'>resume</NavLink></li>
-                    <li><NavLink to='/contact'>contact</NavLink></li>
-                </ul>
-            </nav>
+        <header>
+            <div className='header-container'>
+                <div className='logo'></div>    
+                <nav>
+                    <ul>
+                        <li><NavLink to='/'>home</NavLink></li>
+                        <li><NavLink to='/about'>about</NavLink></li>
+                        <li><NavLink to='/projects'>projects</NavLink></li>
+                        <li><NavLink to='/resume'>resume</NavLink></li>
+                        <li><NavLink to='/contact'>contact</NavLink></li>
+                    </ul>
+                </nav>
 
-            <div className='theme-toggle' onClick={toggleMode}>
-                {mode === '' ? <IoMdMoon className='toggle-icon'/> : <FiSun className='toggle-icon'/>}
+                <div className='theme-toggle' onClick={toggleMode}>
+                    {mode === '' ? <IoMdMoon className='toggle-icon'/> : <FiSun className='toggle-icon'/>}
+                </div>
             </div>
-        </div>
+            
+        </header>
     )
 }
 
