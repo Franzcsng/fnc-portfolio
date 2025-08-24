e0384a8df19433084604fd2ac6fbaaf9f88b1d02
Francis Causing <franzcsng23@gmail.com>
2025-08-25 00:04:46 +0800

    Added skills section in Home page

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,9 +1,61 @@
-import './Home.js'
+import './Home.scss'
+import Banner from './banner/Banner.js'
+import {FaHtml5, FaCss3Alt, FaSquareJs, FaReact, FaNodeJs, FaPhp, FaFlutter } from 'react-icons/fa6'
 
 const Home = () =>{
     return (
-        <div>
+        <div className='home-main'>
 
+            <Banner/>
+
+            <div className='skills-section'>
+
+                <div className='skills-header'>
+                    <hr></hr>
+                    <h2>MY SKILLS</h2>
+                    <hr></hr>
+                </div>
+                
+                <div className='skills-container'>
+                    
+                    <div className='skill-icon'>
+                        <FaHtml5 className='icon'/>
+                        <p>HTML</p>
+                    </div>
+
+                    <div className='skill-icon'>
+                        <FaCss3Alt className='icon'/>
+                        <p>CSS</p>
+                    </div>
+
+                    <div className='skill-icon'>
+                        <FaSquareJs className='icon'/>
+                        <p>JAVASCRIPT</p>
+                    </div>
+
+                    <div className='skill-icon'>
+                        <FaReact className='icon'/>
+                        <p>REACT.JS</p>
+                    </div>
+
+                    <div className='skill-icon'>
+                        <FaNodeJs className='icon'/>
+                        <p>NODE.JS</p>
+                    </div>
+
+                    <div className='skill-icon'>
+                        <FaPhp className='icon'/>
+                        <p>PHP</p>
+                    </div>
+
+                    <div className='skill-icon'>
+                        <FaFlutter className='icon'/>
+                        <p>FLUTTER</p>
+                    </div>
+
+                </div>
+                
+            </div>
         </div>
     )
 }
