e06cc26cddb2b5ec055ae4f5b90d6024799b94e5
Francis Causing <franzcsng23@gmail.com>
2025-08-25 20:13:16 +0800

    Separated Skill list sectioninto a compononent

---
+++ frontend/src/pages/home/SkillList.js
@@ -0,0 +1,56 @@
+import './SkillList.scss'
+import {
+    FaHtml5, 
+    FaCss3Alt, 
+    FaSquareJs, 
+    FaReact, 
+    FaNodeJs, 
+    FaPhp, 
+    FaFlutter 
+} from 'react-icons/fa6'
+
+
+const SkillList = () => {
+    return (
+         <div className='skills-container'>
+                    
+                    <div className='skill-icon'>
+                        <FaHtml5 className='icon'/>
+                        <p>HTML</p>
+                    </div>
+
+                    <div className='skill-icon'>
+                        <FaCss3Alt className='icon'/>
+                        <p>CSS</p>
+                    </div>
+
+                    <div className='skill-icon'>
+                        <FaSquareJs className='icon'/>
+                        <p>JAVASCRIPT</p>
+                    </div>
+
+                    <div className='skill-icon'>
+                        <FaReact className='icon'/>
+                        <p>REACT.JS</p>
+                    </div>
+
+                    <div className='skill-icon'>
+                        <FaNodeJs className='icon'/>
+                        <p>NODE.JS</p>
+                    </div>
+
+                    <div className='skill-icon'>
+                        <FaPhp className='icon'/>
+                        <p>PHP</p>
+                    </div>
+
+                    <div className='skill-icon'>
+                        <FaFlutter className='icon'/>
+                        <p>FLUTTER</p>
+                    </div>
+
+                </div>
+    )
+}
+
+export default SkillList
