646dc59ee4bca7025721e3e10120d0ad9e256a1a
Francis Causing <franzcsng23@gmail.com>
2025-08-27 00:16:28 +0800

    Added about section in Home page

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -35,8 +35,31 @@ const Home = () =>{
                 
                 <ProjectsList/>
                 
-            
             </div>
+
+            <div className='about-section'>
+
+                <div className='about-header'>
+                    <hr></hr>
+                    <h2>ABOUT ME</h2>
+                    <hr></hr>
+                </div>
+
+                <div className='about-container'>
+                    <div className='grid-child text'>
+                        <p>I am a passionate and driven individual with a strong interest in technology, leadership, and continuous growth. With experience in both academic and organizational settings, I enjoy taking on challenges that allow me to apply creativity, problem-solving, and collaboration. I thrive in environments where I can contribute innovative ideas, learn from others, and make a meaningful impact. Beyond my professional and academic pursuits, I value teamwork, adaptability, and a commitment to excellence in everything I do.
+                        </p>
+                    </div>
+
+                    <div className='grid-child images'>
+                        <img src='https://images.unsplash.com/photo-1755177510462-bf74c129428f'></img>
+                    </div>
+                    
+
+                </div>
+            </div>
+
+
         </div>
     )
 }
