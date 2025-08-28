dad3e98ce70d176163a0e32cee81d9d852b6bff5
Francis Causing <franzcsng23@gmail.com>
2025-08-29 01:21:11 +0800

    Added footer content and layout with responsiveness

--- frontend/src/components/layout/footer/Footer.js
+++ frontend/src/components/layout/footer/Footer.js
@@ -1,9 +1,36 @@
 import './Footer.scss'
+import { SiGmail } from "react-icons/si";
+import { FaLinkedin, FaInstagram  } from "react-icons/fa";
 
 const Footer = () => {
     return(
         <div className='footer'>
+            
+            <div className='footer-content'>
 
+                <div className='footer-section'>
+                    <h2>FNC.dev</h2>
+                    <hr></hr>
+                    <p>Web design and development</p>
+                </div>
+                
+                <div className='footer-section'>
+                    <h2>Connect with me</h2>
+                    <p>franzcsng23@gmail.com</p>
+                    <p>+63 928 520 0815</p>
+                </div>
+
+                <div className='footer-section'>
+                    <h2>My Links</h2>
+                    <div className='links'>
+                        <SiGmail className='icon'/>
+                        <FaLinkedin className='icon'/>
+                        <FaInstagram className='icon'/>
+
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
