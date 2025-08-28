f1df95b788bab9f3eb3f04573d77eb9ff25bc95d
Francis Causing <franzcsng23@gmail.com>
2025-08-29 01:23:02 +0800

    Added projects page banner and project cards

--- frontend/src/pages/projects/Projects.js
+++ frontend/src/pages/projects/Projects.js
@@ -1,8 +1,109 @@
 import './Projects.scss'
-
+import GridTiles from '../home/banner/GridTiles.js'
 const Projects = () => {
     return(
-        <div>
+        <div className="projects-main">
+
+            <div className='projects-banner'>
+
+                <div className='grid-container'>
+                    <GridTiles/>
+                </div>
+                
+                <div className='overlay'>
+                    <h1>MY WORKS</h1>
+                    <p>Some are for practice, some are for fun, and some for professional work! Explore my diverse set of projects</p>
+                </div>
+                
+            </div>
+
+            <div className='projects-section'>
+
+                <div className='project-container'>
+                    <h2 >PROJECT 1</h2>
+                    <div className='project-grid'>
+                        
+                        <div className='grid-child'>
+                            <img src='https://images.unsplash.com/photo-1755177510462-bf74c129428f'></img>
+                        </div>
+                        <div className='grid-child info'>
+                            <p>PROJECT 1: A POS SYSTEM</p>
+                            <p>DIFFICULTY: BEGINNER</p>
+                            <p>My first ever POS system. Created with Node.js, React, and express, it was quite a challenging project, but definitely improved my skills for more complex projects. CLick LEARN MORE for a more detailed report.</p>
+                        </div>
+                        <div className='grid-child tech'>
+                            <p>Tech used</p>
+                        </div>
+
+                    </div>
+
+                    <h2 >PROJECT 1</h2>
+                </div>
+
+                <div className='project-container'>
+                    <h2 >PROJECT 1</h2>
+                    <div className='project-grid'>
+                        
+                        <div className='grid-child'>
+                            <img src='https://images.unsplash.com/photo-1755177510462-bf74c129428f'></img>
+                        </div>
+                        <div className='grid-child info'>
+                            <p>PROJECT 1: A POS SYSTEM</p>
+                            <p>DIFFICULTY: BEGINNER</p>
+                            <p>My first ever POS system. Created with Node.js, React, and express, it was quite a challenging project, but definitely improved my skills for more complex projects. CLick LEARN MORE for a more detailed report.</p>
+                        </div>
+                        <div className='grid-child tech'>
+                            <p>Tech used</p>
+                        </div>
+
+                    </div>
+
+                    <h2 >PROJECT 1</h2>
+                </div>
+
+                <div className='project-container'>
+                    <h2 >PROJECT 1</h2>
+                    <div className='project-grid'>
+                        
+                        <div className='grid-child'>
+                            <img src='https://images.unsplash.com/photo-1755177510462-bf74c129428f'></img>
+                        </div>
+                        <div className='grid-child info'>
+                            <p>PROJECT 1: A POS SYSTEM</p>
+                            <p>DIFFICULTY: BEGINNER</p>
+                            <p>My first ever POS system. Created with Node.js, React, and express, it was quite a challenging project, but definitely improved my skills for more complex projects. CLick LEARN MORE for a more detailed report.</p>
+                        </div>
+                        <div className='grid-child tech'>
+                            <p>Tech used</p>
+                        </div>
+
+                    </div>
+
+                    <h2 >PROJECT 1</h2>
+                </div>
+
+                <div className='project-container'>
+                    <h2 >PROJECT 1</h2>
+                    <div className='project-grid'>
+                        
+                        <div className='grid-child'>
+                            <img src='https://images.unsplash.com/photo-1755177510462-bf74c129428f'></img>
+                        </div>
+                        <div className='grid-child info'>
+                            <p>PROJECT 1: A POS SYSTEM</p>
+                            <p>DIFFICULTY: BEGINNER</p>
+                            <p>My first ever POS system. Created with Node.js, React, and express, it was quite a challenging project, but definitely improved my skills for more complex projects. CLick LEARN MORE for a more detailed report.</p>
+                        </div>
+                        <div className='grid-child tech'>
+                            <p>Tech used</p>
+                        </div>
+
+                    </div>
+
+                    <h2 >PROJECT 1</h2>
+                </div>
+
+            </div>
 
         </div>
     )
