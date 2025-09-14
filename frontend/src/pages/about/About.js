24eab21992eff034370174832f3257675deef5db
Francis Causing <franzcsng23@gmail.com>
2025-09-14 14:24:20 +0800

    Refractor: Add logic to render reusable components in about page

--- frontend/src/pages/about/About.js
+++ frontend/src/pages/about/About.js
@@ -1,9 +1,51 @@
 import './About.scss'
 import Banner from './banner/Banner.js'
 import Journey from './Journey.js'
+import AboutItem from './AboutItem.js'
 import { HiAcademicCap } from "react-icons/hi2";
 
 const About = () => {
+
+    let journeys = [
+        "THE BEGINNING",
+        "LEARNING",
+        "EXPERIENCE",
+        "PASSION",
+        "WHAT'S NEXT"
+        ]
+
+    let aboutSections = [
+        {
+            section: "THE BEGINNING",
+            description: "",
+            icon: HiAcademicCap,
+            year: "2002=2010",
+            text: "Behind the Red Gate Under the Santol Tree"
+        },
+    ]
+
+    const returnJourneys = (journeys) => {
+        return journeys.map((journey, index) =>  ( 
+            <Journey
+                key={index}
+                journey={journey}
+            />
+        ))
+    }
+     
+    const returnSections = (sections) => {
+        return sections.map((section, index) => (
+            <AboutItem
+                key={index}
+                section={section.section}
+                description={section.description}
+                icon={section.icon}
+                year={section.year}
+                text={section.text}
+            />
+        ))
+    }
+
     return (
         <div className='about-main'>
 
@@ -19,33 +61,15 @@ const About = () => {
 
                 <div className='journey-list'>
 
-                    <Journey
-                        journey='THE BEGINNING'
-                    />
-
-                    <Journey
-                        journey='LEARNING'
-                    />
-
-                    <Journey
-                        journey='EXPERIENCE'
-                    />
-
-                    <Journey
-                        journey='PASSION'
-                    />
-
-                    <Journey
-                        journey="WHAT'S NEXT"
-                    />
-
+                    {returnJourneys(journeys)}
+                
                 </div>
             </div>
 
             <div className='about-section'>
 
-            
-                
+                {returnSections(aboutSections)}
+
             </div>
         </div>
     )
