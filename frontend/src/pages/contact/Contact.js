b95cc83392fd1be2f32f79a8922a940d554a829a
Francis Causing <franzcsng23@gmail.com>
2025-09-17 15:40:34 +0800

    Refractor: Extract contact-card from contact page into reusable component

--- frontend/src/pages/contact/Contact.js
+++ frontend/src/pages/contact/Contact.js
@@ -1,8 +1,45 @@
 import './Contact.scss'
 import TechIcon from '../../components/common/tech-icon/TechIcon.js'
+import ContactCard from './contact-card/ContactCard.js'
 import { MdOutlineEmail } from "react-icons/md";
+import { FaGithub } from "react-icons/fa";
 
 const Contact = () => {
+
+    let contacts = [
+        {
+            title: 'EMAIL',
+            address: 'franzcsng23@gmail.com',
+            icon: MdOutlineEmail,
+        },
+        {
+            title: 'GITHUB',
+            address: 'franzcsng23@gmail.com',
+            icon: MdOutlineEmail,
+        },
+        {
+            title: 'LINKEDIN',
+            address: 'franzcsng23@gmail.com',
+            icon: MdOutlineEmail,
+        },
+        {
+            title: 'LOCATION',
+            address: 'BACOLOD CITY, PHILIPPINES',
+            icon: MdOutlineEmail,
+        },
+    ]
+
+    const displayContacts = (list) => {
+        return list.map((contact, index) => (
+                <ContactCard
+                    key='index'
+                    icon={contact.icon}
+                    title={contact.title}
+                    address={contact.address}
+                />  
+            ))
+        }
+    
     return (
         <div className='contact-main'>
             <div className='contact-section'>
@@ -10,102 +47,27 @@ const Contact = () => {
                 <div className='banner'>
                     <h1> GET IN TOUCH</h1>
 
-                    <p>
+                    <p className='banner-body'>
                         HAVE A QUESTION, AN IDEA, OR JUST WANT TO SAY HI? DON’T HESITATE TO DROP ME A MESSAGE! I’D BE HAPPY TO CONNECT AND CHAT WITH YOU.
                     </p>
 
+
+                
                     <div className='contact-list'>
 
-                        <div className='contact-card'>
-
-                            <TechIcon
-                                icon={MdOutlineEmail}
-                                size='clamp(30px, 3.7vw, 60px)'
-                            />
-
-
-                            <div className='text'>
-                                <p className='card-title'>
-                                    GITHUB
-                                </p>
-
-                                <p className='address'>
-                                    franzcsng23@gmail.com
-                                </p>
-                            </div>
-                            
-                        </div>
-
-                        <div className='contact-card'>
-
-                            <TechIcon
-                                icon={MdOutlineEmail}
-                                size='clamp(30px, 3.7vw, 60px)'
-                            />
-
-
-                            <div className='text'>
-                                <p className='card-title'>
-                                    GITHUB
-                                </p>
-
-                                <p className='address'>
-                                    franzcsng23@gmail.com
-                                </p>
-                            </div>
-                            
-                        </div>
-
-                        <div className='contact-card'>
-
-                            <TechIcon
-                                icon={MdOutlineEmail}
-                                size='clamp(30px, 3.7vw, 60px)'
-                            />
-
-
-                            <div className='text'>
-                                <p className='card-title'>
-                                    GITHUB
-                                </p>
-
-                                <p className='address'>
-                                    franzcsng23@gmail.com
-                                </p>
-                            </div>
-                            
-                        </div>
-
-                        <div className='contact-card'>
-
-                            <TechIcon
-                                icon={MdOutlineEmail}
-                                size='clamp(30px, 3.7vw, 60px)'
-                            />
-
-
-                            <div className='text'>
-                                <p className='card-title'>
-                                    GITHUB
-                                </p>
-
-                                <p className='address'>
-                                    franzcsng23@gmail.com
-                                </p>
-                            </div>
-                            
-                        </div>
-
-                        
-                    
-                    </div>
+                        { displayContacts(contacts)}
+                                 
+                    </div> 
                 </div>
 
 
             </div>
 
             <div className='contact-section second'>
-
+                <form className='contact-form'>
+                    <label for='subject'> SUBJECT </label>
+                    <input type='text' id='subject' name='subject' placeholder='What do you want to talk about?'></input>
+                </form>
 
             </div>
         </div>
