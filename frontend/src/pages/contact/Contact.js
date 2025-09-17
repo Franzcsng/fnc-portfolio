1f3854d83435b4f73089dee77299928a53bde5ae
Francis Causing <franzcsng23@gmail.com>
2025-09-17 22:39:57 +0800

    Add: Add contact form with complete responsiveness

--- frontend/src/pages/contact/Contact.js
+++ frontend/src/pages/contact/Contact.js
@@ -64,9 +64,35 @@ const Contact = () => {
             </div>
 
             <div className='contact-section second'>
+
                 <form className='contact-form'>
-                    <label for='subject'> SUBJECT </label>
-                    <input type='text' id='subject' name='subject' placeholder='What do you want to talk about?'></input>
+
+                    <h2>CONTACT US</h2>
+                    <p>DON'T BE SHY TO LEAVE A MESSAGE, I'LL GET BACK TO YOU AS SOON AS POSSIBLE!</p>
+
+                    <div className='form-grid'>
+
+                        <div className='form-child'>
+
+                            <label for='name'> NAME </label>
+                            <input type='name' className='first-item' id='name' name='name' placeholder="Who's this from? "></input>
+
+                            <label for='email'> EMAIL </label>
+                            <input type='email' id='email' name='email' placeholder="Your email address here"></input>
+
+                        </div>
+
+                        <div className='form-child'>
+
+                            <label for='message'> MESSAGE </label>
+                            <textarea type='message' id='message' name='message' placeholder="What do you want to talk about? "></textarea>
+
+                        </div>
+
+                    </div>
+
+                    <button className='submit-btn'> SUBMIT </button>
+
                 </form>
 
             </div>
