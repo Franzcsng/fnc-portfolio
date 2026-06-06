39b36eba31206d29eac529b2d8bdf9046ce8ce4d
Francis Causing <franzcsng23@gmail.com>
2026-06-06 22:33:40 +0800

    Port variable in server mismatch

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use('/api/contact', contactRoute);
 
 
 const PORT = process.env.PORT || 5000
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
